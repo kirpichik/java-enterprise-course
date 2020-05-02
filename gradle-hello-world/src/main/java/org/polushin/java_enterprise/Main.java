e5a882bfa8b7115c124d5611e3af68051de621f1
kpolushin <kpolushin@azul.com>
2020-05-02 16:00:47 +0700

    Create task 1.1 with hello world logging

---
+++ gradle-hello-world/src/main/java/org/polushin/java_enterprise/Main.java
@@ -0,0 +1,11 @@
+package org.polushin.java_enterprise;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  public static void main(String[] args) {
+    Logger logger = LoggerFactory.getLogger(Main.class);
+    logger.info("Hello World");
+  }
+}
