117f2457b01c27dc751158e6a798222085bfb20a
kpolushin <kpolushin@azul.com>
2020-05-04 18:38:56 +0700

    Fix Main identation for osm-jaxb

--- osm-jaxb/src/main/java/org/polushin/java_enterprise/osm_jaxb/Main.java
+++ osm-jaxb/src/main/java/org/polushin/java_enterprise/osm_jaxb/Main.java
@@ -4,25 +4,25 @@ import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
 
 public final class Main {
-    public static void main(final String[] args) {
-        final var file = Main.class.getClassLoader().getResourceAsStream("RU-NVS.osm");
-        try (final var wrapper = new XMLReaderWrapper(file)) {
-            printStatistics(StatisticsCollector.collectStatistics(wrapper.getReader()));
-        } catch (final XMLStreamException | JAXBException e) {
-            e.printStackTrace();
-        }
+  public static void main(final String[] args) {
+    final var file = Main.class.getClassLoader().getResourceAsStream("RU-NVS.osm");
+    try (final var wrapper = new XMLReaderWrapper(file)) {
+      printStatistics(StatisticsCollector.collectStatistics(wrapper.getReader()));
+    } catch (final XMLStreamException | JAXBException e) {
+      e.printStackTrace();
     }
+  }
 
-    private static void printStatistics(final StatisticsCollector.Statistics statistics) {
-        System.out.println("Users changes:");
-        statistics.usersChanges.entrySet().stream()
-                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
-                .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+  private static void printStatistics(final StatisticsCollector.Statistics statistics) {
+    System.out.println("Users changes:");
+    statistics.usersChanges.entrySet().stream()
+            .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+            .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
 
-        System.out.println("Tags by nodes:");
-        statistics.tagsByNodes.entrySet().stream()
-                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
-                .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
-    }
+    System.out.println("Tags by nodes:");
+    statistics.tagsByNodes.entrySet().stream()
+            .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+            .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+  }
 
 }
