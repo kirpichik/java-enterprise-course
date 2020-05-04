a3278d46fc7a133000269ee3031aa841625bb3e9
kpolushin <kpolushin@azul.com>
2020-05-04 16:41:35 +0700

    Initial commit for OSM statistics collector with JAXB

---
+++ osm-jaxb/src/main/java/org/polushin/java_enterprise/osm_jaxb/Main.java
@@ -0,0 +1,27 @@
+package org.polushin.java_enterprise.osm_jaxb;
+
+import javax.xml.stream.XMLStreamException;
+
+public final class Main {
+    public static void main(final String[] args) {
+        final var file = Main.class.getClassLoader().getResourceAsStream("RU-NVS.osm");
+        try (final var wrapper = new XMLReaderWrapper(file)) {
+            printStatistics(StatisticsCollector.collectStatistics(wrapper.getReader()));
+        } catch (final XMLStreamException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printStatistics(final StatisticsCollector.Statistics statistics) {
+        System.out.println("Users changes:");
+        statistics.usersChanges.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+                .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+
+        System.out.println("Tags by nodes:");
+        statistics.tagsByNodes.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+                .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+    }
+
+}
